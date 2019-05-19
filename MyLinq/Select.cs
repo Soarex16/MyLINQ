63bc017d0d3cce77f2194d5102d087c5dc4433e8
Soarex16 <soarex16@gmail.com>
2019-05-19 20:14:53 +0300

    Initial commit

---
+++ MyLinq/Select.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinq {
+    public static partial class Enumerable {
+        public static IEnumerable<TResult> Select<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TResult> selector) {
+
+            if (source == null) {
+                throw new ArgumentNullException("source");
+            }
+
+            if (selector == null) {
+                throw new ArgumentNullException("func");
+            }
+
+            return SelectIterator(source, selector);
+        }
+
+        public static IEnumerable<TResult> SelectIterator<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TResult> selector) {
+
+            foreach (var item in source) {
+                yield return selector(item);
+            }
+        }
+    }
+}
