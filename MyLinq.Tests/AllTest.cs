d1210731da4b73179c253778f4bd307fbcc752c6
Soarex16 <soarex16@gmail.com>
2019-05-25 23:10:57 +0300

    Add new ext methods, tests

---
+++ MyLinq.Tests/AllTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MyLinq.Tests {
+    [TestFixture]
+    public class AllTest {
+
+        [Test]
+        public void All_Empty_False() {
+            int[] arr = {};
+            Func<int, bool> predicate = x => x > 0;
+
+            bool res = arr.All(predicate);
+
+            Assert.That(res, Is.True);
+        }
+
+        [Test]
+        public void All_StringList_True() {
+            List<string> strList = new List<string>() {
+                "test method",
+                "test",
+                "testing",
+                "tested"
+            };
+            Func<string, bool> predicate = s => s.StartsWith("test");
+
+            bool res = strList.All(predicate);
+
+            Assert.That(res, Is.True);
+        }
+
+        [Test]
+        public void All_StringList_False() {
+            List<string> strList = new List<string>() {
+                "test method",
+                "test",
+                "abc",
+                "testing",
+                "tested"
+            };
+            Func<string, bool> predicate = s => s.StartsWith("test");
+
+            bool res = strList.All(predicate);
+
+            Assert.That(res, Is.False);
+        }
+    }
+}
---
+++ MyLinq.Tests/AnyTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MyLinq.Tests {
+    [TestFixture]
+    public class AnyTest {
+
+        [Test]
+        public void Any_EmptyList_False() {
+            var list = new List<int>();
+
+            bool res = list.Any();
+
+            Assert.That(res, Is.False);
+        }
+
+        [Test]
+        public void Any_IntList_True() {
+            var list = new List<int>() {
+                1, 2, 3, 4, 5
+            };
+
+            bool res = list.Any();
+
+            Assert.That(res, Is.True);
+        }
+
+        [Test]
+        public void AnyWithPredicate_IntList_True() {
+            var list = new List<int>() {
+                1, 2, 3, 4, 5
+            };
+            Func<int, bool> predicate = i => i % 2 == 0;
+
+            bool res = list.Any(predicate);
+
+            Assert.That(res, Is.True);
+        }
+
+        [Test]
+        public void AnyWithPredicate_EmptyList_False() {
+            var list = new List<int>();
+            Func<int, bool> predicate = i => i % 2 == 0;
+
+            bool res = list.Any();
+
+            Assert.That(res, Is.False);
+        }
+    }
+}
