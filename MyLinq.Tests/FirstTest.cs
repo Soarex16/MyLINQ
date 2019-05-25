4daae0a8c2ed2c89f4ff992f0b8b94810b557c10
Soarex16 <soarex16@gmail.com>
2019-05-25 21:26:35 +0300

    Add unit tests

---
+++ MyLinq.Tests/FirstTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using MyLinq;
+using NUnit.Framework;
+
+namespace MyLinq.Tests {
+    [TestFixture]
+    public class FirstTest {
+
+        [Test]
+        public void First_Empty_InvalidOperationExceptionThrown() {
+            // Arrange
+            var arr = new int[0];
+
+            // Act
+
+            // I can't separate Act and Assert,
+            // because it throws an exception, that I need to caught
+
+            //Assert
+            Assert.That(() => arr.First(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void FirstWithPredicate_WithoutProperElement_InvalidOperationExceptionThrown() {
+            // Arrange
+            string[] arr = {"test", "fixture", "without", "appropriate", "method"};
+            Func<string, bool> predicate = s => s.Length == 1;
+
+            // Act
+
+            // And again I need to combine Act and Assert
+
+            // Assert
+            Assert.That(() => arr.First(predicate), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void First_PredicateNull_ArgumentNullException() {
+            // Arrange
+            string[] arr = {"test", "fixture", "without", "appropriate", "method"};
+            Func<string, bool> predicate = null;
+
+            // Act
+
+            // Assert
+            Assert.That(() => arr.First(predicate), Throws.ArgumentNullException);
+        }
+    }
+}
